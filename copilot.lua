728024e52e2da3778c3c5abd3e2e976a937fcade
JuanJoseSolorzano <juanjose.solorzano.c@gmail.com>
2025-07-02 19:50:35 -0600

    Added: <copilot.lua> - nvim and copilot file configuration.

---
+++ copilot.lua
@@ -0,0 +1,9 @@
+-- This file is part of the Neovim configuration for managin GitHub Copilot.
+-- It shuld be placed in the `C:\Users\juanj\AppData\Local\nvim\lua\plugins\copilot.lua` directory.
+
+return {
+  {
+    "github/copilot.vim",
+    lazy = false, -- load during startup
+  },
+}
